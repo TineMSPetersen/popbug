0f4a4dd769355aedbe5236c23066dd164e9e5882
Tine MS Petersen <tinemeretesikjaerpetersen@gmail.com>
2025-05-06 19:56:58 +0200

    TailwindCSS and fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,15 @@
+import localFont from "next/font/local";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import "./app.css";
+
+const alpino = localFont({
+  src: '../../public/fonts/Alpino-Variable.woff2',
+  display: 'swap',
+  weight: '100 900',
+  variable: '--font-alpino',
+})
 
 export default function RootLayout({
   children,
@@ -7,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className={alpino.variable} lang="en">
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
