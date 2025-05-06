3af307602ad02571d0e3308b9a9ae42a4d5c96f3
Tine MS Petersen <tinemeretesikjaerpetersen@gmail.com>
2025-05-06 20:44:29 +0200

    Logo and Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,14 @@ import localFont from "next/font/local";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 import "./app.css";
+import Header from "@/components/Header";
 
 const alpino = localFont({
-  src: '../../public/fonts/Alpino-Variable.woff2',
-  display: 'swap',
-  weight: '100 900',
-  variable: '--font-alpino',
-})
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
   children,
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html className={alpino.variable} lang="en">
-      <body>{children}</body>
+      <body className="overflow-x-hidden bg-yellow-300">
+        <Header />
+        <main>{children}</main>
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
