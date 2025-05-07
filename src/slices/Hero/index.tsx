12769d84b5aea7a41fb809cbc290cc7a6d19bc49
Tine MS Petersen <tinemeretesikjaerpetersen@gmail.com>
2025-05-07 16:20:50 +0200

    Hero scroll trigger animation

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -7,12 +7,13 @@ import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSplitter";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 /**
  * Props for `Hero`.
@@ -49,6 +50,31 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       y: 10,
       duration: .6
     })
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+      }
+    });
+
+    scrollTl.fromTo("body", {
+      backgroundColor: "#FDE047",
+    }, {
+      backgroundColor: "#D9F99D",
+      overwrite: "auto",
+    }, .5)
+    .from(".text-side-heading .split-char", {
+      scale: 1.3,
+      y: 14,
+      rotate: -25,
+      opacity: 0,
+      stagger: .1,
+      ease: "back.out(3)",
+      duration: .5,
+    });
   })
 
   return (
